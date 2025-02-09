68c605430874561aee7e9e004a21ddb327681f6d
thisranger <t.willemsen.dekkers@gmail.com>
2025-02-09 10:03:10 +0100

    Test

--- src/app.rs
+++ src/app.rs
@@ -1,109 +1,59 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use eframe::egui;
+use ehttp::Request;
+use std::sync::{Arc, Mutex};
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+pub struct MyApp {
+    response: Arc<Mutex<Option<String>>>,
 }
 
-impl Default for TemplateApp {
-    fn default() -> Self {
+impl MyApp {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            response: Arc::new(Mutex::new(None)),
         }
     }
-}
 
-impl TemplateApp {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+    fn send_led_request(&self, url: &str) {
+        let response_clone = Arc::clone(&self.response);
 
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
-        Default::default()
-    }
-}
-
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
-            egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
+        let request = Request::get(url);
+        ehttp::fetch(request, move |result| {
+            let message = match result {
+                Ok(response) => format!("Success: {}", response.status),
+                Err(err) => format!("Error: {}", err),
+            };
+            *response_clone.lock().unwrap() = Some(message);
         });
+    }
 
+    fn turn_led_on(&self) {
+        self.send_led_request("http://tw/led/on"); // Replace with your ESP32 IP
+    }
+
+    fn turn_led_off(&self) {
+        self.send_led_request("http://tw/led/off"); // Replace with your ESP32 IP
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.label("LED Control");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            if ui.button("Turn ON").clicked() {
+                self.turn_led_on();
             }
 
-            ui.separator();
+            if ui.button("Turn OFF").clicked() {
+                self.turn_led_off();
+            }
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            match &*self.response.lock().unwrap() {
+                Some(text) => ui.label(text),
+                None => ui.label("Waiting for response..."),
+            };
         });
-    }
-}
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
+        ctx.request_repaint(); // Ensure UI updates when response changes
+    }
 }
